e3a304253ab056701c54c0309d2a62effa3aec6a
BO YU, Cheng <nemo1999.eecs06@g2.nctu.edu.tw>
2022-11-01 05:10:48 +0800

    create graph and visualizatoin

---
+++ graph.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from typing import Set, Dict, Iterable, Tuple
+# undirected graph
+
+class Graph:
+    def __init__(self) -> None:
+        self.edges : Dict[str, Set[str]] = defaultdict(set) # map each node index to the set of its neibours' indexes
+        self.nodes : Set[str] = set()
+
+    def node(self, node: str) -> None:
+        self.nodes.add(node)
+
+    def edge(self, node1: str, node2:str) -> None:
+        self.edges[node1].add(node2)
+        self.edges[node2].add(node1)
+
+    def edgeDel(self, node1:str , node2: str) -> None:
+        self.edges[node1].remove(node2)
+        self.edges[node2].remove(node1)
+
+    def nodeDel(self, node) -> None:
+        if node in self.nodes:
+            self.nodes.remove(node)
+            for n in self.edges[node]:
+                self.edges[n].remove(node)
+            del self.edges[node]
+    """
+    def addEdges(self, pairs: Iterable[Tuple[str,str]]) -> None:
+        map(lambda n1, n2: self.edge(n1,n2), pairs)
+
+    """
+    
