0e87d47a6e8a798521337701fa6da74272268e52
BO YU, Cheng <nemo1999.eecs06@g2.nctu.edu.tw>
2022-11-01 13:13:06 +0800

    add neighbors(), degree(), clone() to Graph class

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
 from typing import Set, Dict, Iterable, Tuple
+import copy
 # undirected graph
 
 class Graph:
-    def __init__(self) -> None:
+    def __init__(self, prototype: Graph) -> None:
         self.edges : Dict[str, Set[str]] = defaultdict(set) # map each node index to the set of its neibours' indexes
         self.nodes : Set[str] = set()
 
@@ -24,9 +25,19 @@ class Graph:
             for n in self.edges[node]:
                 self.edges[n].remove(node)
             del self.edges[node]
-    """
+    
     def addEdges(self, pairs: Iterable[Tuple[str,str]]) -> None:
         map(lambda n1, n2: self.edge(n1,n2), pairs)
 
-    """
+    def neighbors(self, node: str) -> None:
+        return self.edges[node]
+
+    def degree(self, node: str) -> None:
+        return len(self.edges[node])
+
+    def clone(self) -> Graph: 
+        return copy.deepcopy(self)
+
+
+
     
